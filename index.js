198217e218ee2f963cb98354a3cd3f270746ca14
technosportong <technosportong@gmail.com>
2026-03-24 19:13:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const userRoute=require('./Routes/UserRoute');
 const itemsRoute=require('./Routes/ItemsRoute');
 const billRoute=require('./Routes/BillRoutes');
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://technosportongole.vercel.app",
   credentials: true
 }));
 app.use(express.json());
